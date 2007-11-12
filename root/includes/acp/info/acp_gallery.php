bc35b6b77d49164bb7d235cd4c2898739875698e
Joas Schilling <nickvergessen@gmx.de>
2007-11-12 14:05:25 +0000

    updated root/includes/acp/info/acp_gallery.php for release

--- root/includes/acp/info/acp_gallery.php
+++ root/includes/acp/info/acp_gallery.php
@@ -15,7 +15,7 @@ class acp_gallery_info
 		return array(
 			'filename'	=> 'acp_gallery',
 			'title'		=> 'PHPBB_GALLERY',
-			'version'	=> '0.0.1',
+			'version'	=> '0.1.3',
 			'modes'		=> array(
 				'overview'						=> array('title' => 'ACP_GALLERY_OVERVIEW', 					'auth' => 'acl_a_board', 'cat' => array('PHPBB_GALLERY')),
 				'manage_albums'					=> array('title' => 'ACP_GALLERY_MANAGE_ALBUMS', 				'auth' => 'acl_a_board', 'cat' => array('PHPBB_GALLERY')),
@@ -23,14 +23,12 @@ class acp_gallery_info
 				'configure_gallery'				=> array('title' => 'ACP_GALLERY_CONFIGURE_GALLERY', 			'auth' => 'acl_a_board', 'cat' => array('PHPBB_GALLERY')),
 				'album_permissions'				=> array('title' => 'ACP_GALLERY_ALBUM_PERMISSIONS', 			'auth' => 'acl_a_board', 'cat' => array('PHPBB_GALLERY')),
 				'album_personal_permissions'	=> array('title' => 'ACP_GALLERY_ALBUM_PERSONAL_PERMISSIONS', 	'auth' => 'acl_a_board', 'cat' => array('PHPBB_GALLERY')),
-				)
+				),
 			);
 	}
-							
 	function install()
 	{
 	}
-								
 	function uninstall()
 	{
 	}
