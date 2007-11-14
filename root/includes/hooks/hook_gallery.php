56cb7934ab58b1209b795ef72a31f07e9968e840
Joas Schilling <nickvergessen@gmx.de>
2007-11-14 21:40:14 +0000

    using the hooks system for less edits on core-code

--- root/includes/hooks/hook_gallery.php
+++ root/includes/hooks/hook_gallery.php
@@ -9,24 +9,25 @@
 *
 */
 
-if ( !defined('IN_PHPBB') )
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
 {
-	die('Hacking attempt');
+	exit;
 }
+$user->add_lang('mods/info_acp_gallery');
 
 define('PAGE_ALBUM', -19);// for Session Handling
-
 define('PERSONAL_GALLERY', 0);// pic_cat_id <- do NOT change this value
 
-
 // User Levels for Album system <- do NOT change these values
+define('ALBUM_USER', 0);
 define('ALBUM_ANONYMOUS', 1);
 define('ALBUM_GUEST', 1);
-
-define('ALBUM_USER', 0);
-define('ALBUM_ADMIN', 4);
 define('ALBUM_MOD', 2);
 define('ALBUM_PRIVATE', 3);
+define('ALBUM_ADMIN', 4);
 
 define('ADMIN', 1);
 define('MOD', 2);
@@ -35,7 +36,6 @@ define('MOD', 2);
 // Path (trailing slash required)
 define('ALBUM_UPLOAD_PATH', 'upload/');
 define('ALBUM_CACHE_PATH', 'upload/cache/');
-
 define('ALBUM_DIR_NAME', 'gallery/');
 
 
