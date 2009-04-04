5a131dd2548800b1a58690887354c9d9d2a20c4c
Joas Schilling <nickvergessen@gmx.de>
2009-04-04 09:59:31 +0000

    Be compatible in all ways of phpBB

---
+++ root/gallery/includes/phpbb_functions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* @package phpBB Gallery
+* @version $Id$
+* @copyright (c) 2007 nickvergessen nickvergessen@gmx.de http://www.flying-bits.org
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+/**
+* @ignore
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+/**
+* This file contains functions, to be backwards and forwards compatible with phpBB-versions
+*/
+
+
+?>
