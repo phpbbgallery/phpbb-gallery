d6bf367b1847a8b41dc6bcb20ba2754e4fd731cc
Joas Schilling <nickvergessen@gmx.de>
2009-04-16 13:43:14 +0000

    similar name for all mod-titles

--- root/includes/acp/info/acp_gallery_albums.php
+++ root/includes/acp/info/acp_gallery_albums.php
@@ -17,7 +17,7 @@ class acp_gallery_albums_info
 	{
 		return array(
 			'filename'	=> 'acp_gallery_albums',
-			'title'		=> 'ACP_GALLERY_ALBUM_MANAGEMENT',
+			'title'		=> 'PHPBB_GALLERY',
 			'version'	=> '1.0.0',
 			'modes'		=> array(
 				'manage'	=> array('title' => 'ACP_GALLERY_MANAGE_ALBUMS', 'auth' => 'acl_a_gallery_albums', 'cat' => array('PHPBB_GALLERY')),
--- root/includes/acp/info/acp_gallery_permissions.php
+++ root/includes/acp/info/acp_gallery_permissions.php
@@ -17,7 +17,7 @@ class acp_gallery_permissions_info
 	{
 		return array(
 			'filename'	=> 'acp_gallery_permissions',
-			'title'		=> 'ACP_GALLERY_ALBUM_PERMISSIONS',
+			'title'		=> 'PHPBB_GALLERY',
 			'version'	=> '1.0.0',
 			'modes'		=> array(
 				'manage'	=> array('title' => 'ACP_GALLERY_ALBUM_PERMISSIONS',	'auth' => 'acl_a_gallery_albums',	'cat' => array('PHPBB_GALLERY')),
