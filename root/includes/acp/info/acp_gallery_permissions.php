73a2a347118938a8e566030da77a01c79f71fab2
Joas Schilling <nickvergessen@gmx.de>
2009-04-16 13:37:38 +0000

    Fix: [Change] Move permission-management into an extra file

--- root/includes/acp/info/acp_gallery_permissions.php
+++ root/includes/acp/info/acp_gallery_permissions.php
@@ -17,7 +17,7 @@ class acp_gallery_permissions_info
 	{
 		return array(
 			'filename'	=> 'acp_gallery_permissions',
-			'title'		=> 'ALBUM_AUTH_TITLE',
+			'title'		=> 'ACP_GALLERY_ALBUM_PERMISSIONS',
 			'version'	=> '1.0.0',
 			'modes'		=> array(
 				'manage'	=> array('title' => 'ACP_GALLERY_ALBUM_PERMISSIONS',	'auth' => 'acl_a_gallery_albums',	'cat' => array('PHPBB_GALLERY')),
