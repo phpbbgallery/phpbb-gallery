0786ed2b9be5d222fb024533390bf390f6e7a25f
Joas Schilling <nickvergessen@gmx.de>
2009-06-29 09:23:24 +0000

    nitpicks

--- root/gallery/includes/functions_posting.php
+++ root/gallery/includes/functions_posting.php
@@ -31,7 +31,7 @@ function upload_image(&$image_data)
 		'image_user_id'			=> $user->data['user_id'],
 		'image_user_colour'		=> $user->data['user_colour'],
 		'image_username'		=> $image_data['username'],
-		'image_username_clean'	=> utf8_clean_string(image_data['username']),
+		'image_username_clean'	=> utf8_clean_string($image_data['username']),
 		'image_user_ip'			=> $user->ip,
 		'image_time'			=> $image_data['image_time'],
 		'image_album_id'		=> $image_data['image_album_id'],
