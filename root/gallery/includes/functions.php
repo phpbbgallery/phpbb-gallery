bac01c34e838aa7d16228a6e29b414908cd6e5a3
Joas Schilling <nickvergessen@gmx.de>
2009-08-14 09:29:19 +0000

    [Fix] phpbb::#46315

--- root/gallery/includes/functions.php
+++ root/gallery/includes/functions.php
@@ -130,7 +130,7 @@ function set_gallery_config_count($config_name, $increment, $is_dynamic = false)
 	switch ($db->sql_layer)
 	{
 		case 'firebird':
-			$sql_update = 'CAST(CAST(config_value as integer) + ' . (int) $increment . ' as CHAR)';
+			$sql_update = 'CAST(CAST(config_value as integer) + ' . (int) $increment . ' as VARCHAR(255))';
 		break;
 
 		case 'postgres':
