d34101d87b32afe5abdeff14b217ea36be3364d3
Joas Schilling <nickvergessen@gmx.de>
2010-01-27 16:39:52 +0000

    tag v1.0.5

--- root/adm/mods/phpbb_gallery_version.php
+++ root/adm/mods/phpbb_gallery_version.php
@@ -25,7 +25,7 @@ class phpbb_gallery_version
 			'author'	=> 'nickvergessen',
 			'title'		=> 'phpBB Gallery',
 			'tag'		=> 'phpbb_gallery',
-			'version'	=> '1.0.5',
+			'version'	=> '1.0.5-RC1',
 			'file'		=> array('www.flying-bits.org', 'updatecheck', 'phpbb_gallery.xml'),
 		);
 	}
