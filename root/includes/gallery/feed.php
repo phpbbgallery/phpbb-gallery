a5fdbcf3fe65cff54ddc46f44ade53c4b023810f
Joas Schilling <nickvergessen@gmx.de>
2011-08-24 15:01:24 +0200

    [ticket/871] Feed guid needs to be unique

--- root/includes/gallery/feed.php
+++ root/includes/gallery/feed.php
@@ -180,7 +180,7 @@ class phpbb_gallery_feed
 			echo '<item>';
 			echo '<title>' . $title . '</title>';
 			echo '<link>' . $url_imagepage . '</link>';
-			echo '<guid>' . $row['image_time'] . '</guid>';
+			echo '<guid>' . $url_imagepage . '</guid>';
 			echo '<description>&lt;img src="' . $u_thumbnail . '" alt="" /&gt;&lt;br /&gt;<![CDATA[' . $description;
 			echo '<p>' . $user->lang['STATISTICS'] . ': ' . $image_username . ' ' . $this->separator_stats . ' ' . $user->format_date($row['image_time']) . '</p>';
 			echo ']]></description>';
