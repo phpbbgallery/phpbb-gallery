5d59224d40f58dbc1e1a5ded0ce4379d79e074a5
Joas Schilling <nickvergessen@gmx.de>
2012-03-14 13:20:59 +0100

    [ticket/944] Add dev-tool to detect overwritten language keys

---
+++ root/develop/lfcd/lang.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @package Language File Conflict Detector
+* @version $Id$
+* @copyright (c) 2012 nickvergessen nickvergessen@gmx.de http://www.flying-bits.org
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*
+*/
+
+$lfcd_lang = array(
+	'LFCD_TESTING_FILES'		=> "[b][size=150]Testing language files:[/size][/b]\n%s",
+	'LFCD_CONFLICTS'			=> '[b][size=150]Conflicts:[/size][/b]',
+	'LFCD_STATISTIC'			=> '[b][size=150]Statistic:[/size][/b]',
+	'LFCD_STATISTIC_FILE'		=> '[b]%1$s:[/b] [b][color=red]%2$s[/color][/b] Fails, [b][color=orange]%3$s[/color][/b] Warnings, [b][color=blue]%4$s[/color][/b] Notices, [b][color=purple]%5$s[/color][/b] Infos',
+	'LFCD_STATISTIC_PERMISSION'	=> '[b]%1$s:[/b] [color=#008080]Permission files are currently not supported![/color]',
+	'LFCD_CONFLICT'				=> 'MOD´s language file [b]"%1$s"[/b] conflicts with phpBB´s language file [b]"%2$s"[/b] on key [b]"%3$s"[/b]:' . "\n". 'MOD:[code]%4$s[/code]phpBB:[code]%5$s[/code]',
+	'LFCD_FAIL_RESULT'			=> 'Fail',
+	'LFCD_INFO_RESULT'			=> 'Info',
+	'LFCD_NOTICE_RESULT'		=> 'Notice',
+	'LFCD_PERMISSION_MESSAGE'	=> 'Permission files are currently not supported!',
+	'LFCD_PERMISSION_RESULT'	=> 'Note',
+	'LFCD_WARNING_RESULT'		=> 'Warning',
+);
