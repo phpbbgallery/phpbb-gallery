a4ea6dbdf03e54f66f7b27160762ee742ddb942f
Joas Schilling <nickvergessen@gmx.de>
2014-03-15 23:08:29 +0100

    Remove unneccessary event code and fix header

--- core/event/main_listener.php
+++ core/event/main_listener.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
-*
-* @package NV Newspage Extension
-* @copyright (c) 2013 nickvergessen
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-*
-*/
+ *
+ * @package phpBB Gallery
+ * @copyright (c) 2014 nickvergessen
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
 
 namespace phpbbgallery\core\event;
 
@@ -69,68 +69,5 @@ class main_listener implements EventSubscriberInterface
 		$this->template->assign_vars(array(
 			'U_GALLERY'	=> $this->helper->route('phpbbgallery_index'),
 		));
-	}
-
-	public function add_newspage_viewonline($event)
-	{
-		global $forum_data;
-
-		$route = $this->helper->generate_route();
-		if ($event['on_page'][1] == 'app')
-		{
-			if ($event['row']['session_page'] === 'app.' . $this->php_ext . '/news')
-			{
-				$event['location_url'] = $route->get_url();
-				$event['location'] = $this->user->lang('VIEWONLINE_NEWS');
-			}
-			else if (($forum_id = $this->get_category_from_route($event['row']['session_page'])) &&
-				($archive = $this->get_archive_from_route($event['row']['session_page'])))
-			{
-				$archive_start = $this->user->get_timestamp_from_format('Y/n-d H:i:s', $archive . '-01 0:00:00');
-
-				$event['location_url'] = $route->get_url($forum_id, $archive);
-				$event['location'] = $this->user->lang('VIEWONLINE_NEWS_CATEGORY_ARCHIVE', $forum_data[$forum_id]['forum_name'], $this->user->format_date($archive_start, 'F Y'));
-			}
-			else if ($forum_id = $this->get_category_from_route($event['row']['session_page']))
-			{
-				$event['location_url'] = $route->get_url($forum_id);
-				$event['location'] = $this->user->lang('VIEWONLINE_NEWS_CATEGORY', $forum_data[$forum_id]['forum_name']);
-			}
-			else if ($archive = $this->get_archive_from_route($event['row']['session_page']))
-			{
-				$archive_start = $this->user->get_timestamp_from_format('Y/n-d H:i:s', $archive . '-01 0:00:00');
-
-				$event['location_url'] = $route->get_url(false, $archive);
-				$event['location'] = $this->user->lang('VIEWONLINE_NEWS_ARCHIVE', $this->user->format_date($archive_start, 'F Y'));
-			}
-		}
-	}
-
-	protected function get_category_from_route($route)
-	{
-		$route_ary = explode('/', $route);
-
-		if (sizeof($route_ary) >= 3 && $route_ary[0] === 'app.' . $this->php_ext && $route_ary[1] === 'news' && $route_ary[2] === 'category')
-		{
-			return (int) $route_ary[3];
-		}
-
-		return false;
-	}
-
-	protected function get_archive_from_route($route)
-	{
-		$route_ary = explode('/', $route);
-
-		if (sizeof($route_ary) >= 4 && $route_ary[0] === 'app.' . $this->php_ext && $route_ary[1] === 'news' && $route_ary[2] === 'archive')
-		{
-			return (int) $route_ary[3] . '/' . (int) $route_ary[4];
-		}
-		else if (sizeof($route_ary) >= 6 && $route_ary[0] === 'app.' . $this->php_ext && $route_ary[1] === 'news' && $route_ary[4] === 'archive')
-		{
-			return (int) $route_ary[5] . '/' . (int) $route_ary[6];
-		}
-
-		return false;
 	}
 }
